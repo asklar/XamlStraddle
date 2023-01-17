b184d2a7e6b7ca01480c6782982806dbf3cfc59a
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-01-17 10:57:48 -0800

    Add additional headers

---
+++ inc/UI.Xaml.Automation.Provider.h
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#ifdef USE_WINUI3
+#include <winrt/Microsoft.UI.Xaml.Automation.Provider.h>
+#else
+#include <winrt/Windows.UI.Xaml.Automation.Provider.h>
+#endif //  USE_WINUI3
---
+++ inc/UI.Xaml.Data.h
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#ifdef USE_WINUI3
+#include <winrt/Microsoft.UI.Xaml.Data.h>
+#else
+#include <winrt/Windows.UI.Xaml.Data.h>
+#endif //  USE_WINUI3
