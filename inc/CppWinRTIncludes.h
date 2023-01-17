e3ef827c556c870ca17e93404b2e338e47ce6e13
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-01-17 10:57:09 -0800

    Spaces

--- inc/CppWinRTIncludes.h
+++ inc/CppWinRTIncludes.h
@@ -17,10 +17,11 @@ namespace xaml = winrt::Windows::UI::Xaml;
 namespace comp = winrt::Windows::UI::Composition;
 namespace ui = winrt::Windows::UI;
 namespace winrt {
-namespace system = winrt::Windows::System;
-using ColorHelper = winrt::Windows::UI::ColorHelper;
-using Colors = winrt::Windows::UI::Colors;
+    namespace system = winrt::Windows::System;
+    using ColorHelper = winrt::Windows::UI::ColorHelper;
+    using Colors = winrt::Windows::UI::Colors;
 } // namespace winrt
+
 #else
 
 #include <winrt/Microsoft.System.h>
@@ -31,23 +32,23 @@ namespace xaml = winrt::Microsoft::UI::Xaml;
 namespace comp = winrt::Microsoft::UI::Composition;
 namespace ui = winrt::Microsoft::UI;
 namespace winrt {
-namespace system = winrt::Microsoft::System;
-using ColorHelper = winrt::Microsoft::UI::ColorHelper;
-using Colors = winrt::Microsoft::UI::Colors;
+    namespace system = winrt::Microsoft::System;
+    using ColorHelper = winrt::Microsoft::UI::ColorHelper;
+    using Colors = winrt::Microsoft::UI::Colors;
 } // namespace winrt
 
 namespace winrt::Microsoft::UI::Xaml {
-using IUIElement7 = UIElement;
-using IUIElement9 = UIElement;
-using IUIElement10 = UIElement;
+    using IUIElement7 = UIElement;
+    using IUIElement9 = UIElement;
+    using IUIElement10 = UIElement;
 } // namespace winrt::Microsoft::UI::Xaml
 
-#endif
+#endif // USE_WINUI3
 
 namespace winrt {
-using namespace Windows::UI::Core;
-using namespace Windows::Foundation;
-using namespace Windows::Foundation::Collections;
+    using namespace Windows::UI::Core;
+    using namespace Windows::Foundation;
+    using namespace Windows::Foundation::Collections;
 } // namespace winrt
 
 #define _QUOTE(x) L#x
